c8ffb6d898a6529a2cc50ca6aa5a2d6fa0497c94
Rubensrvsc <rubens.rvsc@gmail.com>
2022-08-16 10:37:28 -0300

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,23 @@
+require 'sidekiq'
+require 'sidekiq/web'
+
+Sidekiq::Web.use ActionDispatch::Cookies
+Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: "_interslice_session"
+Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
+  # Protect against timing attacks:
+  # - See https://codahale.com/a-lesson-in-timing-attacks/
+  # - See https://thisdata.com/blog/timing-attacks-against-string-comparison/
+  # - Use & (do not use &&) so that it doesn't short circuit.
+  # - Use digests to stop length information leaking
+  Rack::Utils.secure_compare(::Digest::SHA256.hexdigest(user), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USER"])) &
+  Rack::Utils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
+end
+
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  mount Sidekiq::Web => '/sidekiq'
+  get '/home', to: 'home#index'
+end
