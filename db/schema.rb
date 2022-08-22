f1f5429d0b19e4e3315782ac259bfcdd02cb695a
rubensrvsc <rubensspfc100@gmail.com>
2022-08-22 20:40:30 -0300

    refactor: add user to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_15_225654) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_19_131848) do
   create_table "books", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name_book"
     t.integer "pages"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
