f2549ab5a374a3d479dc0a8880e90dcfe1833313
rubensrvsc <rubensspfc100@gmail.com>
2022-09-01 21:32:46 -0300

    feat: set controller

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -1,11 +1,23 @@
 class BookController < ApplicationController
-  before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_request, only: %i[index create update destroy]
 
   def index
     render json: Book.all.select(:name_book, :pages)
   end
   
   def create
+    Workers::CreateBookWorker.perform_async(params[:name_book], params[:pages])
+    render json: { status: 'User created' }
+  end
+
+  def update
+    Business::UpdateBook.new.call(id_book: params[:id], name_book: params[:name_book], pages: params[:pages])
+    render json: { status: 'Book Updated' }
+  end
+
+  def destroy
+    Business::DeleteBook.new.call(id_book: params[:id])
+    render json: { status: 'Book deleted' }
   end
 
   private
