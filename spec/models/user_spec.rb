8750ca278b2f7727caed10d6459a471c857ce06b
rubensrvsc <rubensspfc100@gmail.com>
2022-09-02 18:22:41 -0300

    test: add model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.create!(
+        name: Faker::Name.name,
+        username: Faker::Name.name,
+        email: Faker::Internet.email,
+        password: 'wxcvbn'
+      )
+    end
+
+    context 'testing user model' do
+      it { expect(User.count).to eq(1) }
+
+      it { expect(User.first.name).to eq(@user.name) }
+    end
 end
