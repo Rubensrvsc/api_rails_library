cdd3c9ff36589a44649a88ad82072e095122d7b0
rubensrvsc <rubensspfc100@gmail.com>
2022-09-08 20:10:37 -0300

    test: find by id

--- spec/domain/business/repository_spec.rb
+++ spec/domain/business/repository_spec.rb
@@ -5,9 +5,14 @@ require 'rails_helper'
 RSpec.describe Business::Repository do
   before(:each) do
     create_list(:book, 10)
+    @id_book = Book.first.id
     @repository = described_class.new
   end
   context 'repository' do
     it { expect(@repository.all_books.size).to eq(10) }
+
+    it { expect(@repository.find_by_id(id_book: 0)).to be_nil }
+
+    it { expect(@repository.find_by_id(id_book: @id_book)).to be_kind_of(Book) }
   end
 end
