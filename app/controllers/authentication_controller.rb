c4ada3419044c131fd70c065533a7dea020c426f
rubensrvsc <rubensspfc100@gmail.com>
2022-09-16 18:39:34 -0300

    refactor: skip verify ability

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -2,6 +2,7 @@
 
 class AuthenticationController < ApplicationController
   skip_before_action :authenticate_request, only: %i[login sign_up]
+  skip_before_action :current_ability, only: %i[login sign_up]
 
   def login
     @user = User.find_by_username(params[:username])
