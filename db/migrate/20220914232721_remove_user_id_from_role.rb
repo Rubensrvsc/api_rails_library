2d582fed176dfb7061b52b4c505a3eb27a7bf35a
rubensrvsc <rubensspfc100@gmail.com>
2022-09-16 18:37:10 -0300

    test: add database test

--- db/migrate/20220914232721_remove_user_id_from_role.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserIdFromRole < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :roles, :user_id
-  end
-end
--- db/migrate/20220914234018_fix_type.rb
+++
@@ -1,5 +0,0 @@
-class FixType < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :roles, :type, :type_role
-  end
-end
